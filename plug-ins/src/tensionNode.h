d7f847f711411c0f790676de29f67771df086f5f
anno <anno_schachner@gmx.at>
2018-07-30 18:08:48 +0200

    update

--- plug-ins/src/tensionNode.h
+++ plug-ins/src/tensionNode.h
@@ -6,6 +6,8 @@
 #include <maya/MFnDependencyNode.h>
 #include <maya/MFnNumericAttribute.h>
 #include <maya/MFnTypedAttribute.h>
+// #include <maya/MFnRampAttribute.h>
+#include <maya/MRampAttribute.h>
 #include <maya/MFnMeshData.h>
 #include <maya/MPlugArray.h>
 #include <maya/MItMeshPolygon.h>
@@ -32,13 +34,16 @@ public:
     static MStatus initialize();
 
     static MDoubleArray getEdgeLen( const MDataHandle& inMesh );
-    static MStatus setColor( const MObject& outMesh );
+    // static MStatus setColor( const MObject& outMesh );
+
 
 public:
     static MTypeId id;
-    static MObject origShape;
-    static MObject deformedShape;
-    static MObject outShape;
+    static MObject aOrigShape;
+    static MObject aDeformedShape;
+    static MObject aOutShape;
+    static MObject aColorRamp;
+
     static bool isOrigDirty;
     static bool isDeformedDirty;
     static MDoubleArray origEdgeLenArray;
