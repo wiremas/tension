b24426cd8a2e4be904017416d6ba5bf3b5bb4379
anno <anno_schachner@gmx.at>
2018-07-31 09:40:49 +0200

    add postConstructor

--- plug-ins/src/tensionNode.h
+++ plug-ins/src/tensionNode.h
@@ -6,14 +6,12 @@
 #include <maya/MFnDependencyNode.h>
 #include <maya/MFnNumericAttribute.h>
 #include <maya/MFnTypedAttribute.h>
-// #include <maya/MFnRampAttribute.h>
 #include <maya/MRampAttribute.h>
 #include <maya/MFnMeshData.h>
 #include <maya/MPlugArray.h>
 #include <maya/MItMeshPolygon.h>
 #include <maya/MItMeshVertex.h>
 #include <maya/MItMeshEdge.h>
-
 
 // Macros
 #define MCheckStatus(status,message)        \
@@ -22,20 +20,17 @@
                 return status;              \
         }
 
-
 class tensionNode : public MPxNode
 {
 public:
     tensionNode() {}
     virtual ~tensionNode() {}
+    virtual void postConstructor();
     virtual MStatus compute( const MPlug& plug, MDataBlock& data );
     virtual MStatus setDependentsDirty( const MPlug& dirtyPlug, MPlugArray& affectedPlugs );
     static void* creator(){ return new tensionNode(); }
     static MStatus initialize();
-
     static MDoubleArray getEdgeLen( const MDataHandle& inMesh );
-    // static MStatus setColor( const MObject& outMesh );
-
 
 public:
     static MTypeId id;
@@ -50,5 +45,4 @@ public:
     static MDoubleArray deformedEdgeLenArray;
 
 };
-
 
